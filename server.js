a1777bfdc768a3b10d491a7728861333493d4ca4
mayurwagh <mayurwagh1412@gmail.com>
2026-06-04 15:05:44 +0530

    new code is added

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("wele to NGP");
+    res.send(" to NGP");
 });
 
 app.get("/health", (req, res) => {
